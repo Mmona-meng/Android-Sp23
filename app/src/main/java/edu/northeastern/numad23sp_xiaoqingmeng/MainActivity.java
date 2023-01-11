a215c00b791d8e0e64c9593ae988dface01f5508
xmeng <75476123+Mmona-meng@users.noreply.github.com>
2023-01-10 19:09:15 -0800

    complete A1

--- app/src/main/java/edu/northeastern/numad23sp_xiaoqingmeng/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_xiaoqingmeng/MainActivity.java
@@ -3,6 +3,10 @@ package edu.northeastern.numad23sp_xiaoqingmeng;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.aboutMeButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "Name: Xiaoqing Meng Email: meng.xiaoq@northeastern.edu";
+                Log.v("About Me", "Button clicked!");
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
