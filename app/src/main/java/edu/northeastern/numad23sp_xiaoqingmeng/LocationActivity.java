560c6b257ed3df8809165a3720b993c33c789601
xmeng <75476123+Mmona-meng@users.noreply.github.com>
2023-03-18 23:03:17 -0700

    fix screen rotation problem, keep the distance data unchanged

--- app/src/main/java/edu/northeastern/numad23sp_xiaoqingmeng/LocationActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_xiaoqingmeng/LocationActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -24,10 +25,9 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
     private TextView latitude;
     private TextView longitude;
     private TextView distance;
-    private Button resetButton;
-    private Bundle savedState;
     private Location lastLocation;
 
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +36,11 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         latitude = findViewById(R.id.latitude_text_view);
         longitude = findViewById(R.id.longitude_text_view);
         distance = findViewById(R.id.distance_text_view);
-        resetButton = findViewById(R.id.reset_button);
+        Button resetButton = findViewById(R.id.reset_button);
 
         resetButton.setOnClickListener(new View.OnClickListener() {
+            // Sets a click listener on the resetButton that
+            // resets the totalDistance field and updates the distance text view accordingly.
             @Override
             public void onClick(View view) {
                 totalDistance = 0.0;
@@ -46,8 +48,13 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
             }
         });
 
+        // Initializes the locationManager field and requests permission to access the GPS sensor if necessary
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        //If permission is granted, registers the LocationActivity as a listener for GPS updates
+        // and updates the latitude and longitude text views with the user's last known location.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
@@ -57,11 +64,18 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
                 longitude.setText(String.format("%.6f", lastLocation.getLongitude()));
             }
         }
+
+        // If the activity is being recreated, restores the totalDistance field from the saved state.
+        if (savedInstanceState != null) {
+            totalDistance = savedInstanceState.getDouble("totalDistance");
+            distance.setText(String.format("%.3f meters", totalDistance));
+        }
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
     public void onLocationChanged(Location location) {
-        // Called when a new location is found by the network location provider.
+        // Called when the GPS sensor detects a change in the user's location.
         latitude.setText(String.format("%.6f", location.getLatitude()));
         longitude.setText(String.format("%.6f", location.getLongitude()));
         if (lastLocation != null) {
@@ -76,7 +90,7 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
     protected void onPause() {
         // Called when the activity is no longer visible to the user.
         super.onPause();
-        locationManager.removeUpdates(this);
+        locationManager.removeUpdates(this); // Unregisters the LocationActivity as a listener for GPS updates.
     }
 
     @Override
@@ -85,16 +99,18 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         outState.putDouble("totalDistance", totalDistance);
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onResume() {
-        // Called when the activity will start interacting with the user.
+        // Called when the LocationActivity is visible to the user again.
         super.onResume();
-        savedState = getIntent().getExtras();
+        Bundle savedState = getIntent().getExtras();
         if (savedState != null) {
             totalDistance = savedState.getDouble("totalDistance");
             distance.setText(String.valueOf(totalDistance));
         }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
@@ -121,6 +137,7 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
                 .show();
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         // Called when the user has accepted (or denied) the permission request.
